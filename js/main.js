6a5c065c1012ae36ef91e2cc0ae4bf2e22bd4054
Therakar <matteo.pau200@gmail.com>
2022-10-20 17:08:11 +0200

    Chiede e stampa l'età del passegero

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // chiede numero di km che il passeggero ha intenzione di percorrere
-const kmNumber = Number(prompt(`Inserisci il numero di km che hai intenzione di percorrere`));
-
+const kmNumber = Number(prompt(`Inserisci il numero di km che hai intenzione di percorrere:`));
 // stampa l'input nella console
-console.log(kmNumber);
-
+console.log(`Numero km: ` ,kmNumber);
 // stampa l'input nella pagina
 document.getElementById(`kmNumber`).innerHTML = kmNumber;
 
+
+// chiede l'età del passeggero
+const age = Number(prompt(`Inserisci la tua età:`));
+// stampa l'input nella console
+console.log(`Età dell'utente: `,age);
+// stampa l'input nella pagina
+document.getElementById(`age`).innerHTML = age;
 
 
